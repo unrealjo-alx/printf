4e1b590890823fa62e61f6f02dbe32a14c343f0b
Youssef Eddaif <jo9unreal@gmail.com>
2023-04-24 16:42:45 +0100

    fix line been over 80 characters
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- main.h
+++ main.h
@@ -8,6 +8,7 @@ int print_char(char c, int *chars_written);
 int print_string(char *str, int *chars_written);
 int print_percent(int *chars_written);
 int print_error(char invalid_specifier, int *chars_written);
-int handle_specifier(const char **format_ptr, va_list args, int *chars_written);
+int handle_specifier(const char **format_ptr,
+					 va_list args, int *chars_written);
 int _print_int(int n);
 #endif
